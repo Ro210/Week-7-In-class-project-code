3a5f6b71cbbdb67cf0b3c8ddb3caf2adf3b144ef
Ro210 <95069567+Ro210@users.noreply.github.com>
2025-06-10 06:43:14 +0000

    Pending changes exported from your codespace

---
+++ build/CMakeFiles/Week7InclassProjectcode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Week7InclassProjectcode.dir/MyString.cpp.o"
+  "CMakeFiles/Week7InclassProjectcode.dir/main.cpp.o"
+  "Week7InclassProjectcode"
+  "Week7InclassProjectcode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Week7InclassProjectcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
